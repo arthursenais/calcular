1fac26a527818288215c81a061984565ff8c236b
Arthur Sá <100931412+arthursenais@users.noreply.github.com>
2022-09-22 15:25:34 -0300

    switch case

--- calculadora.c
+++ calculadora.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <math.h>
         
 
 int main(int argc, char *argv[]){
-        int a,b,soma,multiplicacao,divisao,subtracao;
+        float a,b,raiz,soma,multiplicacao,divisao,subtracao,potencia;
         
         if(strcmp(argv[1],"-h")==0) {
                 printf("Utilização:\n");
                 printf("%s \t <numero1> <operação> <numero2>\n", argv[0]);
                 printf("\nlista de operações disponíveis:\n");
-                printf("Operações básicas: +, -, *, /\nobs: caso '*' não funcione, utilize-o entre aspas\n");
+                printf("+, -, *, /, ^,\nobs: caso alguma não funcione, utilize-a entre aspas\n");
                 return -1;
         } else if(argc!=4) {
                 printf("Por favor, utilize 3 argumentos\n");
@@ -19,21 +19,27 @@ int main(int argc, char *argv[]){
                 return -1;
 
         }
-        a = atoi(argv[1]);
-        b = atoi(argv[3]);
+        a = atof(argv[1]);
+        b = atof(argv[3]);
         soma = a+b;
         multiplicacao = a*b;
         divisao = a/b;
         subtracao = a-b;
+        potencia = pow(a,b);
+        raiz = pow(a,1/b);
         switch (*argv[2]) {
-                case '+':  printf("%d %s %d = %d\n",a,argv[2],b,soma);
+                case '+': printf("%g %s %g = %g\n",a,argv[2],b,soma);
                         break;
-                case '-': printf("%d %s %d = %d\n",a,argv[2],b,subtracao);
+                case '-': printf("%g %s %g = %g\n",a,argv[2],b,subtracao);
                         break;
-                case '*': printf("%d %s %d = %d\n",a,argv[2],b,multiplicacao);
+                case '*': printf("%g %s %g = %g\n",a,argv[2],b,multiplicacao);
                         break;
-                case '/': printf("%d %s %d = %d\n",a,argv[2],b,divisao);
+                case '/': printf("%g %s %g = %g\n",a,argv[2],b,divisao);
                         break;
+                case '^': printf("%g %s %g = %g\n",a,argv[2],b,potencia);
+                        break;
+                case 'r': printf("%g %s %g = %g\n",a,argv[2],b,raiz);
+                break;
         default:
                 printf("Operação \"%s\" não reconhecida. Utilize \"%s -h\" para ajuda \n",argv[2],argv[0]);
                 return -1;
