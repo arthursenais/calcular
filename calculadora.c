e887ffd1f2076a39b3a08d30f7bf0d66c17f85a4
Arthur Sá <100931412+arthursenais@users.noreply.github.com>
2022-09-22 13:56:41 -0300

    Add files via upload

--- calculadora.c
+++ calculadora.c
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]){
         if(strcmp(argv[1],"-h")==0) {
                 printf("Utilização:\n");
                 printf("%s \t <numero1> <operação> <numero2>\n", argv[0]);
-                printf("lista de operações disponíveis:\n");
-                printf("Operações básicas: +, -, x, /\n");
+                printf("\nlista de operações disponíveis:\n");
+                printf("Operações básicas: +, -, *, /\nobs: caso '*' não funcione, utilize-o entre aspas\n");
                 return -1;
         } else if(argc!=4) {
                 printf("Por favor, utilize 3 argumentos\n");
@@ -25,15 +25,16 @@ int main(int argc, char *argv[]){
         multiplicacao = a*b;
         divisao = a/b;
         subtracao = a-b;
-        if (strcmp(argv[2],"+")==0) {
-                printf("%d %s %d = %d\n",a,argv[2],b,soma);
-        } else if (strcmp(argv[2],"x")==0) {
-                printf("%d %s %d = %d\n",a,argv[2],b,multiplicacao);
-        } else if (strcmp(argv[2],"/")==0) {
-                printf("%d %s %d = %d\n",a,argv[2],b,divisao);
-        } else if (strcmp(argv[2],"-")==0) {
-                printf("%d %s %d = %d\n",a,argv[2],b,subtracao);
-        } else {
+        switch (*argv[2]) {
+                case '+':  printf("%d %s %d = %d\n",a,argv[2],b,soma);
+                        break;
+                case '-': printf("%d %s %d = %d\n",a,argv[2],b,subtracao);
+                        break;
+                case '*': printf("%d %s %d = %d\n",a,argv[2],b,multiplicacao);
+                        break;
+                case '/': printf("%d %s %d = %d\n",a,argv[2],b,divisao);
+                        break;
+        default:
                 printf("Operação \"%s\" não reconhecida. Utilize \"%s -h\" para ajuda \n",argv[2],argv[0]);
                 return -1;
         }
